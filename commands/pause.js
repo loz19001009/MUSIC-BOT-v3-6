598e62db48c5a202222ba4d03b38b4ae0ba18ed5
loz19001009 <161129298+loz19001009@users.noreply.github.com>
2024-05-19 11:15:31 +0700

    pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -23,7 +23,7 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "pause",
-  description: "Stops playing the currently playing music.",
+  description: "Dừng việc phát nhạc đang phát.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -32,7 +32,7 @@ module.exports = {
 
     try {
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Không có bài hát nào đang phát!!', ephemeral: true });
       }
 
       const success = queue.pause();
@@ -40,11 +40,11 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#7645fe') 
         .setAuthor({
-          name: 'Song Paused',
+          name: 'Bài hát đã tạm dừng',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157296313013117049/8061-purple-pause-icon.png?ex=651817ae&is=6516c62e&hm=4596c9fab9d8b66de8b5215b2750572ced352eed67440a1134550b846b5693b9&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-        .setDescription(success ? '**The music has been Paused for a moment!!**' : '❌ Command Error: Unable to pause song')
+        .setDescription(success ? '**Nhạc đã tạm dừng một lát!!**' : '❌ Lỗi lệnh: Không thể tạm dừng bài hát')
         
 
       return interaction.reply({ embeds: [embed] });
@@ -53,6 +53,7 @@ module.exports = {
     }
   },
 };
+
 
 /*
 
