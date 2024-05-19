0793ad739df6fa02d2ef8159eaaf582012c7db8a
loz19001009 <161129298+loz19001009@users.noreply.github.com>
2024-05-19 10:58:40 +0700

    stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -23,7 +23,7 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "stop",
-  description: "Stops the music.",
+  description: "Dừng nhạc.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -31,7 +31,7 @@ module.exports = {
     try {
       const queue = client.player.getQueue(interaction.guild.id);
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Không có nhạc đang phát!!', ephemeral: true });
       }
 
       queue.stop(interaction.guild.id);
@@ -39,12 +39,11 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#f1002c')
         .setAuthor({
-          name: 'Music Stopped',
+          name: 'Dừng Nhạc',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157305318255116400/pngtree-vector-stop-icon-png-image_4233262.jpg?ex=65182011&is=6516ce91&hm=d5a8ca6010716bae836b025f8d36557a95f14c13a705f65eb09a54161649c795&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-        .setDescription('**The journey stops, but the rhythm lives on.**')
-        
+        .setDescription('**Hành trình dừng lại, nhưng nhịp sống mãi mãi.**')
 
       return interaction.reply({ embeds: [embed] });
     } catch (e) {
@@ -52,6 +51,7 @@ module.exports = {
     }
   },
 };
+
 
 
 /*
