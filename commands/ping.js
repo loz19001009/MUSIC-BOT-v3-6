e444b87c0f6ee8cfdba450f48310c64500188f7b
loz19001009 <161129298+loz19001009@users.noreply.github.com>
2024-05-19 11:18:08 +0700

    ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -22,7 +22,7 @@ const { EmbedBuilder } = require('discord.js')
 const db = require("../mongoDB");
 module.exports = {
   name: "ping",
-  description: "check the bot latency",
+  description: "Kiểm tra độ trễ của bot",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
@@ -31,11 +31,11 @@ module.exports = {
     try {
 
       const start = Date.now();
-      interaction.reply("Pinging....").then(msg => {
+      interaction.reply("Đang ping....").then(msg => {
         const end = Date.now();
         const embed = new EmbedBuilder()
           .setColor(`#6190ff`)
-          .setTitle(`Bot Latency`)
+          .setTitle(`Độ Trễ của Bot`)
           .setDescription(`**Pong** : ${end - start}ms`)
         return interaction.editReply({ embeds: [embed] }).catch(e => { });
       }).catch(err => { })
@@ -45,6 +45,7 @@ module.exports = {
   }
   },
 };
+
 /*
 
   ________.__                        _____.___.___________
