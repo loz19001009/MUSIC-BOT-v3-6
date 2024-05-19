b2296ac260d128701578c498481382b92765887a
loz19001009 <161129298+loz19001009@users.noreply.github.com>
2024-05-19 11:14:55 +0700

    nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -2,28 +2,28 @@ const { EmbedBuilder } = require('discord.js');
 const db = require("../mongoDB");
 module.exports = {
   name: "nowplaying",
-  description: "get info of current song.",
+  description: "Xem thông tin bài hát hiện tại.",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
     try {
 
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ Không có bài hát nào đang phát!!`, ephemeral: true }).catch(e => { })
 
       const track = queue.songs[0];
-      if (!track) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+      if (!track) return interaction.reply({ content: `⚠️ Không có bài hát nào đang phát!!`, ephemeral: true }).catch(e => { })
 
       const embed = new EmbedBuilder();
       embed.setColor(client.config.embedColor);
       embed.setThumbnail(track.thumbnail);
       embed.setTitle(track.name)
-      embed.setDescription(`> **Audio** \`%${queue.volume}\`
-> **Duration :** \`${track.formattedDuration}\`
+      embed.setDescription(`> **Âm thanh** \`%${queue.volume}\`
+> **Thời lượng :** \`${track.formattedDuration}\`
 > **URL :** **${track.url}**
-> **Loop Mode :** \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'}\`
-> **Filter**: \`${queue.filters.names.join(', ') || 'Off'}\`
-> **By :** <@${track.user.id}>`);
+> **Chế độ lặp :** \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'Toàn bộ hàng đợi' : 'Bài hát này') : 'Tắt'}\`
+> **Bộ lọc**: \`${queue.filters.names.join(', ') || 'Tắt'}\`
+> **Bởi :** <@${track.user.id}>`);
 
 
       interaction.reply({ embeds: [embed] }).catch(e => { })
@@ -33,3 +33,4 @@ module.exports = {
   }
   },
 };
+
