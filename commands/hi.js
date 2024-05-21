8158b08f8c3a49516164b89d8bf3cabe9d489f68
loz19001009 <161129298+loz19001009@users.noreply.github.com>
2024-05-22 06:57:24 +0700

    hi.js

--- commands/hi.js
+++ commands/hi.js
@@ -3,13 +3,15 @@ const db = require("../mongoDB");
 
 module.exports = {
   name: "hi",
-  description: "Chào Legend_august.",
+  description: "Chào người dùng.",
   permissions: "0x0000000000000800",
   options: [],
 
   run: async (client, interaction) => {
     try {
-      interaction.reply('Chào Legend_august!');
+      const user = interaction.user; // Lấy thông tin người dùng từ interaction
+      const username = user.username; // Lấy tên người dùng
+      interaction.reply(`Chào ${username}!`); // Trả lời bằng tên người dùng
     } catch (e) {
       console.error(e); 
     }
